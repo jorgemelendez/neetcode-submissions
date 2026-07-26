4ca8cc92e39f2f80bcaaa759463915cbdf77927e
Jorge M. <31938581+jorgemelendez@users.noreply.github.com>
2026-07-26 12:10:46 -0600

    Add: lru-cache - submission-0

---
+++ Data Structures & Algorithms/lru-cache/submission-0.java
@@ -0,0 +1,97 @@
+class LRUCache {
+
+    private Map<Integer, ValueHolder> map;
+    private Node head;
+    private Node tail;
+    private int size;
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        this.map = new HashMap<>();
+        this.capacity = capacity;
+    }
+    
+    public int get(int key) {
+        ValueHolder value = map.get(key);
+
+        Node valueNode = value.address;
+        Node lftNode = valueNode.lft;
+        Node rgtNode = valueNode.rgt;
+
+        lftNode.rgt = rgtNode;
+        if (rigtNode != null)
+            rgtNode.lft = lftNode;
+        if (tail == valueNode) {            
+            tail = lftNode;
+        }
+
+        Node prevHead = head;
+        head = valueNode;
+
+        if (prevHead != null) {
+            valueNode.rgt = prevHead;
+            prevHead.lft = head;
+        }
+        
+        return value.value;
+    }
+    
+    public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            ValueHolder valueHolder = map.get(key);
+            // re-arrange the list
+            Node address = valueHolder.address;
+            Node left = address.lft;
+            Node right = address.rgt;
+            left.rgt = right;
+            
+            if (rgt != null) {
+                right.lft = left;
+            }
+
+            Node prevHead = head;
+            this.head = address;
+            adress.rgt = prevHead;
+
+        } else {
+            size++;
+            // create new node and add check if we need to set tail and head
+            Node prevHead = this.head;
+            this.head = new Node();
+            this.head.key = key;
+            this.head.rgt = prevHead;
+
+            if (tail == null) {
+                tail = head;
+            }
+        }
+
+
+        if (size > capacity) {
+            Integer key = tail.key;
+            map.remove(key);
+            tail = tail.lft;
+            tail.rgt = null;
+        }
+    }
+}
+
+private class Node {
+    Node lft;
+    Node rgt;
+    Integer key;
+
+    public Node() {
+
+    }
+}
+
+
+private class ValueHolder {
+    int value;
+    Node address;
+
+    public ValueHolder() {
+
+    }
+}
