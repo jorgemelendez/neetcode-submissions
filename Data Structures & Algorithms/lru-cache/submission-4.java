bd2c2a298881c0c7cc347aa979994b097f74d068
Jorge M. <31938581+jorgemelendez@users.noreply.github.com>
2026-07-26 20:43:13 -0600

    Add: lru-cache - submission-4

---
+++ Data Structures & Algorithms/lru-cache/submission-4.java
@@ -0,0 +1,68 @@
+class LRUCache {
+
+    private Map<Integer, Node> map;
+    private Node head;
+    private Node tail;
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        this.head = new Node(0, 0);
+        this.tail = new Node(0,0);
+        this.map = new HashMap<>();
+        this.capacity = capacity;
+    }
+    
+    public int get(int key) {
+        if (!map.contains(key)) {
+            return -1;
+        }
+
+        Node node = map.get(key);
+        remove(node);
+        insertRecent(node);
+    }
+    
+    public void put(int key, int value) {
+        if (map.contains(key)) {
+            Node node = map.get(key);
+            remove(node);
+            insertRecent(node);
+        } else {
+            Node node = new Node();
+            node.key = key;
+            node.value = value;
+            insertRecent(node);
+        }
+    }
+
+    public void remove(Node node) {
+        map.remove(node.key);
+        node.lft.rgt = node.rgt;
+        node.rgt.lft = node.lft;
+    }
+
+    public void insertRecent(Node node) {
+        if (map.size() > capacity) {
+            remove(tail.lft);
+        }
+
+        node.rgt = this.head.rgt;
+        this.head.rgt = node;
+    }
+}
+
+private class Node {
+    Node lft;
+    Node rgt;
+    Integer key;
+    Integer value;
+
+    public Node() {
+
+    }
+
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
