a1ee1ef6cc6d0797c263681e2e052dbee43b294a
Jorge M. <31938581+jorgemelendez@users.noreply.github.com>
2026-07-31 18:19:37 -0600

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        char[] sChar = s.toCharArray();
+
+        int left = 0, right = sChar.length - 1;
+        int deletionsLeft = 1;
+        while(left <= right) {
+            if (sChar[left] != sChar[right]) {
+                if (deletionsLeft == 0 ) {
+                    return false;
+                }
+
+                // check if next left is 
+                if (sChar[left + 1] == sChar[right]) {
+                    left++;
+                } else if (sChar[left] == sChar[right- 1]) {
+                    right--;
+                } else {
+                    return false;
+                }
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
