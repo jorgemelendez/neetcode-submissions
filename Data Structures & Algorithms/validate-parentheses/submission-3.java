1549e0334552e3d7b6eec223399c9875a981ba94
Jorge M. <31938581+jorgemelendez@users.noreply.github.com>
2026-08-03 08:11:58 -0600

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> q = new ArrayDeque<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            char bracket = s.charAt(i);
+
+            if (bracket == '{' || bracket == '[' || bracket == '(') {
+                q.push(bracket);
+                continue;
+            }
+
+            char openingBracket = q.pop();
+            if (openingBracket == '{' && bracket == '}'
+                || openingBracket == '(' && bracket == ')'
+                || openingBracket == '[' && bracket == ']') {
+                    continue;
+                }
+
+            return false;
+        }
+
+        return q.size() == 0;
+    }
+}
